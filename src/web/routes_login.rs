cc2b5300a7c482a30ca12dd0991917ed141d0b90
rafliputraa <rafli.putra712@gmail.com>
2024-02-15 01:17:58 +0700

    feat: context added, proper logging, err handling for no auth, and enhanced token validation

--- src/web/routes_login.rs
+++ src/web/routes_login.rs
@@ -1,7 +1,6 @@
-use std::ptr::write_bytes;
-use crate::{Error, Result, web};
-use axum::{Json, Router};
+use crate::{web, Error, Result};
 use axum::routing::post;
+use axum::{Json, Router};
 use serde::Deserialize;
 use serde_json::{json, Value};
 use tower_cookies::{Cookie, Cookies};
@@ -10,7 +9,10 @@ pub fn routes() -> Router {
     Router::new().route("/api/login", post(api_login))
 }
 
-async fn api_login(cookies: Cookies, payload: Json<LoginPayload>) -> Result<Json<Value>> {
+async fn api_login(
+    cookies: Cookies,
+    payload: Json<LoginPayload>,
+) -> Result<Json<Value>> {
     println!("->> {:<12} - api_login", "HANDLER");
 
     // TODO: Implement real db/auth logic.
@@ -18,15 +20,18 @@ async fn api_login(cookies: Cookies, payload: Json<LoginPayload>) -> Result<Json
         return Err(Error::LoginFail);
     }
 
-    // FIXME: Implement real auth-token generation/signature
-    cookies.add(Cookie::new(web::AUTH_TOKEN, "user-1.exp.sign"));
+    // FIXME: Implement real auth-token generation/signature.
+    let mut cookie = Cookie::new(web::AUTH_TOKEN, "user-1.exp.sign");
+    cookie.set_http_only(true);
+    cookie.set_path("/");
+    cookies.add(cookie);
 
-    // Create the success body
+    // Create the success body.
     let body = Json(json!({
-        "result": {
-            "success": true
-        }
-    }));
+		"result": {
+			"success": true
+		}
+	}));
 
     Ok(body)
 }
